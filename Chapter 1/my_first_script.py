799c73fa0b72bf89f4b527e7ba4085fe3cd9d58a
Roman Love <romeloves2program@gmail.com>
2022-03-26 10:04:34 -0400

    Update my_first_script.py

--- Chapter 1/my_first_script.py
+++ Chapter 1/my_first_script.py
@@ -2,8 +2,42 @@
 """
 Spyder Editor
 
+ Created on March 26th, 2022
+ 
+ @author: Romeadome
+ 
 This is a temporary script file.
 """
 
 print("This is my first Python Script!")
+
+print("This is my second Python message!")
+
+#End of chapter exercises
+"""
+Add a line of code yo my_first_script.py so that it prints out a third 
+message that says here is a third message!
+"""
+
+print("Here is a third message!")
+
+"""
+What is the output from each of the following Python statements? First write down
+the answers and then run the commands in Spyder to verify
+"""
+
+print(2) #2
+print(3**2) #9
+print(7//3) #2
+print(7/3) #2.33
+print(7%3) #1
+print(2+2) #4
+print(10*2) #20
+
+"""
+What is the command line in the Spyder editor if you want to find the result of
+55 multiplied by 234?
+"""
+
+print(55*234) #12870
 
